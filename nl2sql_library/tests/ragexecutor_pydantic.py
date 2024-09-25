62b624cdc698baf82649bc8b11731707b2444455
sanchit latawa <sanchitlatawa@google.com>
2024-09-25 15:21:52 +0530

    remove redundant rag executor file
    remove redundant rag executor file

--- nl2sql_library/tests/ragexecutor_pydantic.py
+++ nl2sql_library/tests/ragexecutor_pydantic.py
@@ -13,14 +13,14 @@ llm = text_bison_32k()
 dataset_name = "sl-test-project-363109.zoominfo"
 bigquery_connection_string = "bigquery://sl-test-project-363109/zoominfo"
 
-PGPROJ = "sl-test-project-363109"
-PGLOCATION = "us-central1"
-PGINSTANCE = "nl2sql-test"
-PGDB = "test-db"
-PGUSER = "postgres"
-PGPWD = "nl2sql-test"
-project_id = "sl-test-project-363109"
-dataset_id = "sl-test-project-363109.zoominfo"
+PGPROJ = ""
+PGLOCATION = ""
+PGINSTANCE = ""
+PGDB = ""
+PGUSER = ""
+PGPWD = ""
+project_id = ""
+dataset_id = ""
 
 rag_sql_generator = RagSqlGenerator(
     llm=llm,
